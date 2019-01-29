72c93a600f7f649298d4f99f5a4272a29c582974
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-29 16:38:41 -0700

    Set up actions reducers store and default state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <header>
         Hello
+      </header>
       </div>
     );
   }
