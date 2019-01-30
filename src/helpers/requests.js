6b72af6cbea783c7509ea8963788550a4c2f227e
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-30 16:42:40 -0700

    Add testing for fetchMovies

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -2,9 +2,9 @@ import { apikey } from '../apikey';
 
 export const fetchMovies = async () => {
   const url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US&page=1`;
-  const response= await fetch(url);
+  const response = await fetch(url);
   if (response.status >= 300) {
-    throw new Error(`Error fetching', code: ${response.status}`)
+    throw new Error(`Error fetching, code: ${response.status}`)
   } else {
     return response.json();
   }
---
+++ src/helpers/requests.test.js
@@ -0,0 +1,41 @@
+import { fetchMovies } from './requests';
+import { apikey } from '../apikey';
+import { shallow } from 'enzyme';
+import { mockMovieData } from '../__fixtures__/mockData';
+
+describe('fetchMovies', () => {
+  
+  beforeEach(() => {
+    window.fetch = jest.fn();
+  });
+
+  it('should make a fetch request when fetchMovies is invoked', () => {
+    let mockUrl = `https://api.themoviedb.org/3/movie/now_playing?api_key=${apikey}&language=en-US&page=1`;
+    
+    fetchMovies();
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl);
+  });
+
+  it('should get movies if everything is okay', async () => {
+    const expected = {
+      results: mockMovieData
+    }
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      json: () => Promise.resolve(expected),
+        status: 200
+      }));
+
+    const result = await fetchMovies();
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an error is everything is not okay', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 404
+    }));
+
+    const expected = Error('Error fetching, code: 404');
+    await expect(fetchMovies()).rejects.toEqual(expected)
+  });
+});
