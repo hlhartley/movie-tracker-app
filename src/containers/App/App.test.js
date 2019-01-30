fae5c824b19b9e5d7360d9be2e74d63632fdd3e6
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-30 15:51:42 -0700

    Update app test to confirm it renders without crashing

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import rootReducer from '../../reducers';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = createStore(rootReducer)
+  ReactDOM.render(<Provider store={store}><BrowserRouter><App /></BrowserRouter></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
