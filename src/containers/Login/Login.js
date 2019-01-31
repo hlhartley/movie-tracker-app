2a053fb6d63938dbbe6bf3cf1b96f789676d4140
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-01-31 16:30:58 -0700

    Refactor Login to update errorStatus if any issues loggin in

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { getUser } from '../../helpers/requests';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { loginUser } from '../../actions';
+import { loginUser, updateError } from '../../actions';
 
 export class Login extends Component {
     constructor() {
@@ -21,24 +21,26 @@ export class Login extends Component {
 
     handleSubmit = async (e) => {
         e.preventDefault()
-
+        this.props.updateError('');        
         try {
             const result = await getUser(this.state)
             this.props.loginUser(result.data.id, result.data.name)
         } catch(error) {
-            this.setState({ error: error.message })
+            this.props.updateError(error.message);
         }
         
     }
 
     render() {
-        if(!this.props.currentUser) {
+        let { currentUser, errorStatus } = this.props;
+        if(!currentUser) {
             return(
                 <form onSubmit={this.handleSubmit}>
                     <label htmlFor='email'>E-mail</label>
                     <input name='email' value={this.state.email} id='email' onChange={this.handleChange}/>
                     <label htmlFor='password'>Password</label>
                     <input name='password' value={this.state.password} id='password' onChange={this.handleChange}/>
+                    { (errorStatus !== '') && <p>{errorStatus}</p> }
                     <button>Login</button>
                 </form>
             )    
@@ -51,11 +53,13 @@ export class Login extends Component {
 }
 
 export const mapStateToProps = (state) => ({
-    currentUser: state.currentUser
+    currentUser: state.currentUser,
+    errorStatus: state.errorStatus
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-    loginUser: (id, name) => dispatch(loginUser(id, name))
+    loginUser: (id, name) => dispatch(loginUser(id, name)),
+    updateError: (message) => dispatch(updateError(message))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
