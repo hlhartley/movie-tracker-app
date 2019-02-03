47eb8f8b89c9ec32eee90c20fcd96b2d1f2b5157
Heather Hartley <hlhartley@gmail.com>
2019-02-03 13:48:43 -0700

    Connects MovieCard to store

--- src/containers/MovieCard/MovieCard.js
+++ src/containers/MovieCard/MovieCard.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const MovieCard = ({ id, original_title, overview, release_date, vote_average, poster_path }) => {
-    return (
-        <div className='movie-card'>
-            <i class={'fas fa-crown favorite-false'}></i>
-            <Link to={`movies/${id}`}>
-            <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} className='movie-poster' alt={original_title}/>
-            </Link>
-        </div>
-    )
-}
+export class MovieCard extends Component {
+    constructor() {
+        super()
+    }
 
-export default MovieCard
+    render() {
+        let { id, original_title, poster_path, isFavorite, currentUser } = this.props
+        return (
+            
+                <div className='movie-card'>
+                    <i className={'fas fa-crown favorite-' + isFavorite}></i>
+                    <Link to={`movies/${id}`}>
+                        <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} className='movie-poster' alttext={original_title}/>
+                    </Link>
+                </div>
+            )   
+        }    
+    }
+
+
+export const mapStateToProps = (state) => ({
+    currentUser: state.currentUser   
+})
+
+export default connect(mapStateToProps)(MovieCard)
