0f88a1e290919179c47f887788dff9969d29ce6e
Heather Hartley <hlhartley@gmail.com>
2019-02-03 17:26:57 -0700

    Adds functionality to reset favorites array if user logs out

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -1,33 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { logoutUser } from '../../actions';
+import { logoutUser, resetFavorites, toggleFavorite } from '../../actions';
 
-export const Navigation = (props) => {
-    let { currentUser, logoutUser } = props
-    if (currentUser) {
-        return( 
-        <nav>
-            <NavLink to ='/favorites' className='nav'>Favorites </NavLink>
-            <button onClick={() => logoutUser()}>Log out</button>
-        </nav>
-        )
-    } else {
-        return(
+export class Navigation extends Component {
+    constructor() {
+        super()
+    }
+
+    handleLogout = () => {
+        let { favoriteMovies, toggleFavorite, resetFavorites, logoutUser } = this.props
+        if (favoriteMovies.length > 0) {
+            favoriteMovies.forEach((favorite) => {
+                toggleFavorite(favorite);
+            })
+            resetFavorites();
+        }
+        logoutUser();
+    }
+
+    render() {
+        let { currentUser } = this.props
+        if (currentUser) {
+            return( 
             <nav>
-                <NavLink to ='/login' className='nav'>Login</NavLink> 
-                <NavLink to ='/create-account' className='nav'>Create Account</NavLink> 
+                <NavLink to ='/favorites' className='nav'>Favorites </NavLink>
+                <button onClick={this.handleLogout}>Log out</button>
             </nav>
-        )
+            )
+        } else {
+            return(
+                <nav>
+                    <NavLink to ='/login' className='nav'>Login</NavLink> 
+                    <NavLink to ='/create-account' className='nav'>Create Account</NavLink> 
+                </nav>
+            )
+        }
     }
 }
 
 export const mapStateToProps = (state) => ({
-    currentUser: state.currentUser
+    currentUser: state.currentUser,
+    favoriteMovies: state.favoriteMovies,
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-    logoutUser: () => dispatch(logoutUser())
+    logoutUser: () => dispatch(logoutUser()),
+    toggleFavorite: (id) => dispatch(toggleFavorite(id)),
+    resetFavorites: () => dispatch(resetFavorites()) 
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navigation);
