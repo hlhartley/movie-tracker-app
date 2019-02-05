bce93b1866a2c74d710dbc0a0443b6837678552b
Tiffany Bachmann <tiffanyrbachmann@gmail.com>
2019-02-04 17:57:49 -0700

    Update MovieCard display

--- src/containers/MovieInfoCard/MovieInfoCard.js
+++ src/containers/MovieInfoCard/MovieInfoCard.js
@@ -5,7 +5,6 @@ export const MovieInfoCard = ({ title, id, release_date, poster_path, overview }
     <div className='movie-info'>
       <h2>{title}</h2>
       <p>{release_date}</p>
-      <i class={'fas fa-crown favorite-false'}></i>
       <img src={`https://image.tmdb.org/t/p/w500/${poster_path}`}/>
       <p>{overview}</p>
     </div>
